4e7b3cae4af7a6c12f70b869fb2fd00e54fc003a
seattleowl <seattleowl@users.noreply.github.com>
2019-07-22 09:49:37 -0700

    change ViewPort to Viewport

--- test.js
+++ test.js
@@ -8,12 +8,21 @@ const veiwport = new GameSquare.Viewport("", 500, 500, mainScene)
 // OBJECTS
 let playerSquare = new GameSquare.Rectangle({
     pos: {x:10,y:10},
-    size: {width:50,height:50},
+    size: {width:10,height:10},
     color: "red"
 })
 mainScene.add(playerSquare)
 
 // LOGIC
 playerSquare.on("keypress_ArrowRight", (p) => {
-    p.x += 10
+    p.x += 5
+})
+playerSquare.on("keypress_ArrowLeft", (p) => {
+    p.x -= 5
+})
+playerSquare.on("keypress_ArrowUp", (p) => {
+    p.y -= 5
+})
+playerSquare.on("keypress_ArrowDown", (p) => {
+    p.y += 5
 })
